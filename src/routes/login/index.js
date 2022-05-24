2dc51b211a320beff379e6d4abd4681b518a0076
Guilherme <guilhermehallmann@gmail.com>
2022-05-24 18:27:04 -0300

    feat: login controller

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const loginValidation = require('../../middlewares/loginValidation');
+const loginController = require('../../controllers/loginController');
 
 const router = express.Router();
 
-router.post('/', loginValidation);
+router.post('/', loginValidation, loginController);
 
 module.exports = router;
