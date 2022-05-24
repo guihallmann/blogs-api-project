26f010c79e044fe9990f267929bb1774a35f630a
Guilherme <guilhermehallmann@gmail.com>
2022-05-24 13:13:41 -0300

    fix: table names where in lower case, causing the req01 test to break

--- src/database/migrations/20220523170454-create-blog-post.js
+++ src/database/migrations/20220523170454-create-blog-post.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('blogPosts', {
+    await queryInterface.createTable('BlogPosts', {
       id: {
         allowNull: false,
         autoIncrement: true,
--- src/database/migrations/20220523173047-create-post-category.js
+++ src/database/migrations/20220523173047-create-post-category.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('postCategories', {
+    await queryInterface.createTable('PostCategories', {
       postId: {
         type: Sequelize.INTEGER,
         primaryKey: true,
