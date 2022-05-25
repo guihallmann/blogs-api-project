ee6629f8187ce0333af617a4f0a45f156deb7bcc
Guilherme <guilhermehallmann@gmail.com>
2022-05-25 11:05:42 -0300

    feat: creating user post + user Joi validation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const loginRoute = require('./login');
+const userRoute = require('./user');
 
 const router = express.Router();
 
 router.use('/login', loginRoute);
+router.use('/user', userRoute);
 
 module.exports = router;
---
+++ src/routes/user/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// const userValidation = require('../../middlewares/');
+// const userController = require('../../controllers/');
+
+const router = express.Router();
+
+router.post('/');
+
+module.exports = router;
