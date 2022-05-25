f857c5f9c0914675a87854ff2a3df1517e329a6d
Guilherme <guilhermehallmann@gmail.com>
2022-05-25 17:14:07 -0300

    feat: list users and verify token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,7 +13,7 @@ const create = async (req, res, next) => {
 const getAll = async (_req, res, next) => {
   try {
     const users = await userService.getAll();
-    return res.status(200).json({ users });
+    return res.status(200).json(users);
   } catch (err) {
     next(err);
   }
--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -4,8 +4,13 @@ const errorThrow = require('../utils/errorThrow');
 const authToken = (req, _res, next) => {
   const token = req.headers.authorization;
   if (!token) throw errorThrow(401, 'Token not found');
-  verifyJWT.authToken(token);
-  next();
+  
+  try {
+    verifyJWT(token);
+    next();
+  } catch (err) {
+    throw errorThrow(401, 'Expired or invalid token');
+  }
 };
 
 module.exports = authToken;
