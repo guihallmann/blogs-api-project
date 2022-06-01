ab5aee35104aef2232ae453862d1ba214244149a
Guilherme <guilhermehallmann@gmail.com>
2022-06-01 14:13:41 -0300

    feat: creating getById route

--- src/routes/blogPost/index.js
+++ src/routes/blogPost/index.js
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.post('/', tokenValidation, blogPostValidation, blogPostController.create);
 router.get('/', tokenValidation, blogPostController.getAll);
+router.get('/:id', tokenValidation, blogPostController.getById);
 
 module.exports = router;
