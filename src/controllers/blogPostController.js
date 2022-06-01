af4c43878ecfa8a72946ded9cefbeae10a34b214
Guilherme <guilhermehallmann@gmail.com>
2022-06-01 18:09:27 -0300

    fix: didnt deconstruct the id from req.params

--- src/controllers/blogPostController.js
+++ src/controllers/blogPostController.js
@@ -22,7 +22,7 @@ const getAll = async (_req, res, next) => {
 
 const getById = async (req, res, next) => {
   try {
-    const id = req.params;
+    const { id } = req.params;
     const post = await blogPostService.getById(id);
     return res.status(200).json(post);
   } catch (err) {
